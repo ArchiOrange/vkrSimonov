796fe394ab3ea5d062523a19bca91470b8fc54b5
ArchiOrange <dinka4.435@gmail.com>
2020-04-23 15:45:50 +0300

    api for work with subject

---
+++ routes/admin.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+let AdminControllers = require('../controllers/admin')
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Admin panel' });
+});
+router.get('/subjects',AdminControllers.subjects);
+router.get('/addsubjects',AdminControllers.addSubject);
+module.exports = router;
